25b68638750a50b63fa91f76ec7889d0fcfda6ad
jtn <jtneumann@hotmail.com>
2018-07-29 21:48:11 +0000

    minor style changes to login and register views

--- routes/index.js
+++ routes/index.js
@@ -59,8 +59,8 @@ router.post("/login", passport.authenticate("local", {
 
 router.get("/logout", function(req, res) {
     req.logout();
-    req.flash("success", "Logged you out!");
-    res.redirect("/");
+    req.flash("success", "You have been successfully logged out!");
+    res.redirect("/campgrounds");
 });
 
 module.exports = router;
