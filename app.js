48876fa4afe60f52fd183a2e12d55002bd33212b
jtn <jtneumann@hotmail.com>
2018-07-29 19:55:23 +0000

    updated DB ENV info

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v8"
+mongoose.connect(url);
 
-// mongoose.connect("mongodb://localhost/yelp_camp_v8");
-mongoose.connect("mongodb://jtn:aS5c1s2bKo0L@ds259241.mlab.com:59241/yelp-camp")
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
